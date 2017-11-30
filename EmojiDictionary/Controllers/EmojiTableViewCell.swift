cc21afcefcd0fbf1e4ea85c3fad5f617213f7a3a
steph <teststephanezaffran@gmail.com>
2017-11-30 15:09:37 +0200

    add custom cell

---
+++ EmojiDictionary/Controllers/EmojiTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EmojiTableViewCell.swift
+//  EmojiDictionary
+//
+//  Created by steph on 30/11/2017.
+//  Copyright © 2017 ronny abraham. All rights reserved.
+//
+
+import UIKit
+
+class EmojiTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
